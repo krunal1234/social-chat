1b1febe91ee7e626b06a3392d2ef710858cb5903
Krunal Makwana <krunal.sprouto@gmail.com>
2024-08-25 14:40:45 +0530

    sidebar bug

--- src/app/(admin)/app/(pages)/layout.js
+++ src/app/(admin)/app/(pages)/layout.js
@@ -3,11 +3,16 @@ import 'react-toastify/dist/ReactToastify.css';
 import Sidebar from "../components/Sidebar";
 import TopBar from "../components/TopBar";
 import Footer from "../components/Footer";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function RootLayout({ children }) {
   
   const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  const checkViewport = () => {
+    const isMobileOrTablet = window.matchMedia("(max-width: 1024px)").matches;
+    setIsSidebarOpen(!isMobileOrTablet); // Sidebar is closed on mobile and tablet by default
+  };
 
   const handleSidebarToggle = () => {
     setIsSidebarOpen(!isSidebarOpen);
@@ -17,12 +22,21 @@ export default function RootLayout({ children }) {
     setIsSidebarOpen(false);
   };
 
+  useEffect(() => {
+    checkViewport(); // Initial check
+    window.addEventListener('resize', checkViewport); // Update on resize
+
+    return () => {
+      window.removeEventListener('resize', checkViewport); // Cleanup
+    };
+  }, []);
+
   return (
     <div className="h-screen">
-      <div className={`hidden lg:block fixed bg-white inset-y-0 left-0 z-20 transition-transform duration-300 shadow-xl ease-in-out ${isSidebarOpen ? 'translate-x-0 opacity-100 visible' : '-translate-x-full opacity-0 invisible'}`}>
-        <Sidebar/>
+      <div className={`fixed bg-white inset-y-0 left-0 z-20 transition-transform duration-300 shadow-xl ease-in-out ${isSidebarOpen ? 'translate-x-0 opacity-100 visible' : '-translate-x-full opacity-0 invisible'}`}>
+        <Sidebar />
       </div>
-      <div className={`${isSidebarOpen ? 'lg:ml-64' : 'lg:ml-0'} transition-all duration-300 flex flex-col`}>
+      <div className={`${isSidebarOpen ? 'ml-64' : 'ml-0'} transition-all duration-300 flex flex-col`}>
         <TopBar onSidebarToggle={handleSidebarToggle} />
         <div className='min-h-screen bg-gray-100'>
         {children}
