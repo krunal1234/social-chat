84e51112063ebf2946d862b8fe5f2f4381a39231
Makwana Krunal <33956250+krunal1234@users.noreply.github.com>
2024-10-24 18:01:18 +0530

    Update updateSession.js

--- utils/supabase/updateSession.js
+++ utils/supabase/updateSession.js
@@ -1,6 +1,7 @@
 import { createServerClient } from '@supabase/ssr';
 import { NextResponse } from 'next/server';
 
+
 export async function updateSession(request) {
   const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL;
   const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
